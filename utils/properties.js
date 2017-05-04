2b3f2e065f76ef753e5ea77b9a5ccf7f5021cbc6
Joey Miller <miller_joseph3@bah.com>
2017-05-04 12:36:15 -0400

    Moved database update triggers out of bot.js and into utils fetchData and loadData.  Moved all shared variables into properties file.

---
+++ utils/properties.js
@@ -0,0 +1,46 @@
+// const botUserToken = process.env.BOT_TOKEN;
+const botUserToken = "MjM4MDM4Njk1NzM3ODE5MTM4.CugaVA.VxJQUbsOXax0bhrSPdxS3HMRwVs";
+exports.botUserToken = botUserToken;
+// const blizzardAPIKey = process.env.API_KEY;
+const blizzardAPIKey = "9jgj6gnfgr7wjgn6w5db8vpfpatam2uy";
+exports.blizzardAPIKey = blizzardAPIKey;
+const mongodburl = "mongodb://localhost:27017/test";
+exports.mongodburl = mongodburl;
+
+const ilvlApi = "https://us.api.battle.net/wow/character/{realm}/{character}?fields=items&locale=en_US&apikey="+blizzardAPIKey;
+exports.ilvlApi = ilvlApi;
+const itemApi = "https://us.api.battle.net/wow/item/{id}?locale=en_US&apikey="+blizzardAPIKey;
+exports.itemApi = itemApi;
+const statsApi = "https://us.api.battle.net/wow/character/{realm}/{character}?fields=stats&locale=en_US&apikey="+blizzardAPIKey;
+exports.statsApi = statsApi;
+const auctionAPI = "https://us.api.battle.net/wow/auction/data/dalaran?locale=en_US&apikey="+blizzardAPIKey;
+exports.auctionApi = auctionAPI;
+
+const classNames = ["Warrior", "Paladin", "Hunter", "Rogue", "Priest", "Death Knight", "Shaman", "Mage", "Warlock", "Monk", "Druid", "Demon Hunter"];
+exports.classNames = classNames;
+const specNames = {
+    //warrior
+    'Scaleshard': 'Protection', 'Strom\'kar, the Warbreaker': 'Arms', 'Warswords of the Valarjar': 'Fury',
+    //paladin
+    'Oathseeker': 'Protection', 'Ashbringer': 'Retribution', 'The Silver Hand': 'Holy',
+    //hunter
+    'Thas\'dorah, Legacy of the Windrunners': 'Marksmanship', 'Talonclaw': 'Survival', 'Titanstrike': 'Beast Mastery',
+    //rogue
+    'The Kingslayers': 'Assassination', 'The Dreadblades': 'Outlaw', 'Fangs of the Devourer': 'Subtelty',
+    //priest
+    'T\'uure, Beacon of the Naaru': 'Holy', 'Light\'s Wrath': 'Discipline', 'Xal\'atath, Blade of the Black Empire': 'Shadow',
+    //death knight
+    'Maw of the Damned': 'Blood', 'Frostreaper': 'Frost', 'Apocalypse': 'Unholy',
+    //shaman
+    'The Fist of Ra-den': 'Elemental', 'Doomhammer': 'Enhancement', 'Sharas\'dal, Scepter of Tides': 'Restoration',
+    //mage
+    'Aluneth': 'Arcane', 'Felo\'melorn': 'Fire', 'Ebonchill': 'Frost',
+    //warlock
+    'Spine of Thal\'kiel': 'Demonology', 'Scepter of Sargeras': 'Destruction', 'Ulthalesh, the Deadwind Harvester': 'Affliction',
+    //monk
+    'Sheilun, Staff of the Mists': 'Mistweaver', 'Fists of the Heavens': 'Windwalker', 'Fu Zan, the Wanderer\'s Companion': 'Brewmaster',
+    //druid
+    'Fangs of Ashmane': 'Feral', 'Claws of Ursoc': 'Guardian', 'G\'Hanir, the Mother Tree': 'Restoration', 'Scythe of Elune': 'Balance',
+    //demon hunter
+    'Twinblades of the Deceiver': 'Havoc', 'Aldrachi Warblades': 'Vengeance'};
+exports.specNames = specNames;
