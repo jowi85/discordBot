aae0d244ca240b77f353d8e9f2f1d03a4b9d24d9
Morgan Schryver <mschryver@blizzard.com>
2019-01-29 16:35:39 -0800

    Moved code to src/ dir, added Dockerfile and systemd unit file

--- keys.js
+++
@@ -1,9 +0,0 @@
-const BOT_TOKEN = "";
-exports.BOT_TOKEN = BOT_TOKEN;
-const LOG_KEY = "";
-exports.LOG_KEY = LOG_KEY;
-
-const wowClientId = "";
-exports.wowClientId = wowClientId;
-const wowClientSecret = "";
-exports.wowClientSecret = wowClientSecret;
