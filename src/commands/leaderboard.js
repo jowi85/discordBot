55ec9163945fb85a8eb9ffdd60d185b08f93a01b
Joseph Miller <jowimiller@gmail.com>
2019-07-16 10:44:43 -0400

    Added a guildRoster list to include more people for leaderboard calls

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -1,31 +1,20 @@
 const raiderioScore = "https://raider.io/api/v1/characters/profile?region=us&realm=dalaran&name=vname&fields=mythic_plus_scores_by_season%3Acurrent";
 const apiCalls = require('../helpers/apiCalls');
+const guildRoster = require('../helpers/guildRoster');
 
-const members = [
-    {name: 'Tórz'},
-    {name: 'Bearstick'},
-    {name: 'Gynis'},
-    {name: 'Chimlee'},
-    {name: 'Rÿzer'},
-    {name: 'Shaawa'},
-    {name: 'Eeyr'},
-    {name: 'Cedís'},
-    {name: 'Brogainz'},
-    {name: 'Stangmeister'}
-];
 
 module.exports = {
     name: 'leaderboard',
     description: 'leaderboard',
     async execute(msg, args) {
         if (args.length === 1 && (args[0] === '3' || args[0] === '5')) {
-            for (let i = 0; i < members.length; i++) {
-                let body = await apiCalls.callEndpoint(raiderioScore.replace("vname", members[i].name));
-                members[i].score = body.mythic_plus_scores_by_season[0].scores.all;
+            for (let i = 0; i < guildRoster.members.length; i++) {
+                let body = await apiCalls.callEndpoint(raiderioScore.replace("vname", guildRoster.members[i].name));
+                guildRoster.members[i].score = body.mythic_plus_scores_by_season[0].scores.all;
             }
-            members.sort(compare);
+            guildRoster.members.sort(compare);
             for (let i = 0; i < parseInt(args[0]); i++) {
-                msg.channel.send(members[i].name + ": " + members[i].score);
+                msg.channel.send(guildRoster.members[i].name + ": " + guildRoster.members[i].score);
             }
         }
         else {}
