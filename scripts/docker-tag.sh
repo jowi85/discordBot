4856eb4b7b587c9b80241406526eee34378c347f
cronym <crono.jack@gmail.com>
2019-07-16 15:06:26 -0700

    swap to build #

--- scripts/docker-tag.sh
+++ scripts/docker-tag.sh
@@ -3,11 +3,11 @@ set -ev
 
 echo "$DOCKER_PW" | docker login -u "$DOCKER_USER" --password-stdin
 
-docker tag docker.io/cronym/fp-discord-bot:${TRAVIS_PULL_REQUEST} docker.io/cronym/fp-discord-bot:latest
-docker push docker.io/cronym/fp-discord-bot:${TRAVIS_PULL_REQUEST}
+docker tag docker.io/cronym/fp-discord-bot:${TRAVIS_BUILD_NUMBER} docker.io/cronym/fp-discord-bot:latest
+docker push docker.io/cronym/fp-discord-bot:${TRAVIS_BUILD_NUMBER}
 docker push docker.io/cronym/fp-discord-bot:latest
 
 if [[ ! -z "$TRAVIS_TAG" ]]; then
-  docker tag docker.io/cronym/fp-discord-bot:${TRAVIS_PULL_REQUEST} docker.io/cronym/fp-discord-bot:${TRAVIS_TAG}
+  docker tag docker.io/cronym/fp-discord-bot:${TRAVIS_BUILD_NUMBER} docker.io/cronym/fp-discord-bot:${TRAVIS_TAG}
   docker push docker.io/cronym/fp-discord-bot:${TRAVIS_TAG}
 fi
