128614b6af87410f61b424b8689caa86b2c09dec
cronym <crono.jack@gmail.com>
2019-07-16 10:46:27 -0700

    Added version command

---
+++ src/commands/version.js
@@ -0,0 +1,10 @@
+const BOT_VERSION = process.env.BOT_VERSION || "";
+
+module.exports = {
+    name: 'version',
+    description: 'Bot version string',
+    async execute(msg, args) {
+      if (BOT_VERSION.length > 0) {
+        msg.channel.send(`Current bot version: ${BOT_VERSION}`)
+      }
+};
