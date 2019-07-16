d0dca5165b3e5868087121f333aa8973a5127eeb
cronym <crono.jack@gmail.com>
2019-07-16 15:01:45 -0700

    inverted the empty string test on tag

--- scripts/docker-tag.sh
+++ scripts/docker-tag.sh
@@ -7,7 +7,7 @@ docker tag docker.io/cronym/fp-discord-bot:${TRAVIS_PULL_REQUEST} docker.io/cron
 docker push docker.io/cronym/fp-discord-bot:${TRAVIS_PULL_REQUEST}
 docker push docker.io/cronym/fp-discord-bot:latest
 
-if [[ -z "$TRAVIS_TAG" ]]; then
+if [[ ! -z "$TRAVIS_TAG" ]]; then
   docker tag docker.io/cronym/fp-discord-bot:${TRAVIS_PULL_REQUEST} docker.io/cronym/fp-discord-bot:${TRAVIS_TAG}
   docker push docker.io/cronym/fp-discord-bot:${TRAVIS_TAG}
 fi
