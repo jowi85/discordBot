2f9a75c127a154f4fe13e627ad92f0e4b14c89c6
cronym <crono.jack@gmail.com>
2019-07-16 14:57:11 -0700

    fixed docker tag

--- scripts/docker-tag.sh
+++ scripts/docker-tag.sh
@@ -4,10 +4,10 @@ set -ev
 echo "$DOCKER_PW" | docker login -u "$DOCKER_USER" --password-stdin
 
 docker tag docker.io/cronym/fp-discord-bot:${TRAVIS_PULL_REQUEST} docker.io/cronym/fp-discord-bot:latest
-docker push docker.io.cronym/fp-discord-bot:${TRAVIS_PULL_REQUEST}
-docker push docker.io.cronym/fp-discord-bot:latest
+docker push docker.io/cronym/fp-discord-bot:${TRAVIS_PULL_REQUEST}
+docker push docker.io/cronym/fp-discord-bot:latest
 
 if [[ -z "$TRAVIS_TAG" ]]; then
   docker tag docker.io/cronym/fp-discord-bot:${TRAVIS_PULL_REQUEST} docker.io/cronym/fp-discord-bot:${TRAVIS_TAG}
-  docker push docker.io.cronym/fp-discord-bot:${TRAVIS_TAG}
+  docker push docker.io/cronym/fp-discord-bot:${TRAVIS_TAG}
 fi
