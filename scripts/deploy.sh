b0e5d267771f745effcbda16610305e42f134a89
cronym <858628+cronym@users.noreply.github.com>
2019-08-22 14:06:58 -0700

    working deploy on tag... I think

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
+mkdir ~/.kube
 
 cat <<EOF > ~/.kube/config
 ---
@@ -28,7 +29,8 @@ EOF
 
 kubectl config set clusters.vps-fp-discord-bot.certificate-authority-data "$KUBE_CLUSTER_CA"
 kubectl config set clusters.vps-fp-discord-bot.server "$KUBE_HOST"
-kubectl config set users.fp-discord-bot.user.token "$KUBE_SVC_ACCT_TOKEN"
+kubectl config set users.fp-discord-bot.token "$KUBE_SVC_ACCT_TOKEN"
 
 # kubectl set image deployment/fp-discord-bot fp-discord-bot=docker.io/cronym/fp-discord-bot:"${TRAVIS_TAG}"
-echo "kubectl set image deployment/fp-discord-bot fp-discord-bot=docker.io/cronym/fp-discord-bot:\"${TRAVIS_TAG}\""
+kubectl set image deployment/fp-discord-bot "fp-discord-bot=docker.io/cronym/fp-discord-bot:${TRAVIS_TAG}"
+kubectl set env deployments/fp-discord-bot "BOT_VERSION=${TRAVIS_TAG}"
