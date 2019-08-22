ec50d0d4bbc1d99683e6dd5c7b3b32fb8fe09bbb
cronym <858628+cronym@users.noreply.github.com>
2019-08-22 09:06:42 -0700

    testing kube deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+cat <<EOF > ~/.kube/config
+---
+apiVersion: v1
+kind: Config
+users:
+  - name: fp-discord-bot
+    user:
+      token: ""
+clusters:
+  - cluster:
+      server: ""
+      certificate-authority-data: ""
+    name: vps-fp-discord-bot
+preferences: {}
+contexts:
+  - name: bot
+    context:
+      cluster: vps-fp-discord-bot
+      user: fp-discord-bot
+current-context: bot
+EOF
+
+kubectl config set clusters.vps-fp-discord-bot.certificate-authority-data "$KUBE_CLUSTER_CA"
+kubectl config set clusters.vps-fp-discord-bot.server "$KUBE_HOST"
+kubectl config set users.fp-discord-bot.user.token "$KUBE_SVC_ACCT_TOKEN"
+
+# kubectl set image deployment/fp-discord-bot fp-discord-bot=docker.io/cronym/fp-discord-bot:"${TRAVIS_TAG}"
+echo "kubectl set image deployment/fp-discord-bot fp-discord-bot=docker.io/cronym/fp-discord-bot:\"${TRAVIS_TAG}\""
