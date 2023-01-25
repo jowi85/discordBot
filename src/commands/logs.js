a16dae20bfe5d014ad5e1694970fa13739a7d19e
Joey Miller <jowimiller@gmail.com>
2023-01-25 15:44:52 -0500

    changed description text for logs slashcommand

--- src/commands/logs.js
+++ src/commands/logs.js
@@ -7,7 +7,7 @@ const warcraftlogsEndpoint = "https://www.warcraftlogs.com/api/v2/client";
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('logs')
-        .setDescription('Replies with the most recent WarcraftLogs link!'),
+        .setDescription('Replies with FP\'s most recent WarcraftLogs link!'),
     async execute(interaction) {
         let body = await apiCalls.callEndpointWithToken(warcraftlogsEndpoint, warcraftlogsOauth, warcraftlogsClientId, warcraftlogsClientSecret)
         await interaction.reply("https://www.warcraftlogs.com/reports/" + body.data.reportData.reports.data[0].code);
